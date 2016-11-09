727c979b8ea466dc9dc849c5b9d15eb97dfed6dd
4miners <mariusz@serek.net>
2016-11-10 00:10:37 +0100

    Branded for SHIFT.

--- logic/transaction.js
+++ logic/transaction.js
@@ -219,7 +219,7 @@ Transaction.prototype.checkBalance = function (amount, balance, trs, sender) {
 	return {
 		exceeded: exceeded,
 		error: exceeded ? [
-			'Account does not have enough LSK:', sender.address,
+			'Account does not have enough SHIFT:', sender.address,
 			'balance:', bignum(sender[balance].toString() || '0').div(Math.pow(10,8))
 		].join(' ') : null
 	};
